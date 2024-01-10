b7e4f029fabdb0d13655157ff3b33d547a48fd4d
jtod110 <155170941+jtod110@users.noreply.github.com>
2024-01-09 20:46:29 -0500

    Add files via upload

---
+++ src/app/services/geonames.service.spec.ts
@@ -0,0 +1,17 @@
+//I didn't touch this file everything was auto generated when creating the file or added on to by file editor programmer when adding content to other pages
+import { TestBed } from '@angular/core/testing';
+
+import { GeonamesService } from './geonames.service';
+
+describe('GeonamesService', () => {
+  let service: GeonamesService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(GeonamesService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
